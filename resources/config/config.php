179f1df55f612c38f16759afc635c3e0254b887d
Ryan Thompson <ryan@pyrocms.com>
2016-10-14 15:14:59 -0500

    Mode and default value

--- resources/config/config.php
+++ resources/config/config.php
@@ -1,7 +1,18 @@
 <?php
 
 return [
-    'countries' => [
+    'mode'          => [
+        'required' => true,
+        'type'     => 'anomaly.field_type.select',
+        'config'   => [
+            'default_value' => 'input',
+            'options'       => [
+                'input'    => 'anomaly.field_type.country::config.mode.option.input',
+                'dropdown' => 'anomaly.field_type.country::config.mode.option.dropdown',
+            ],
+        ],
+    ],
+    'countries'     => [
         'type'   => 'anomaly.field_type.checkboxes',
         'config' => [
             'default_value' => [
@@ -10,5 +21,8 @@ return [
             'handler'       => 'countries',
             'mode'          => 'tags',
         ],
+    ],
+    'default_value' => [
+        'type' => 'anomaly.field_type.state',
     ],
 ];
